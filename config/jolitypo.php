bd0e1c0ccbb6d5b556c749bed6b3f30c51cd654a
Stefan Graupner <stefan.graupner@gmail.com>
2014-12-19 13:51:06 +0100

    More language defaults + config cleanup

--- config/jolitypo.php
+++ config/jolitypo.php
@@ -1,8 +1,11 @@
 <?php return [
   'defaults' => ['Ellipsis', 'Dimension', 'Dash', 'CurlyQuote', 'Hyphen', 'Trademark'],
   // jolitypo defaults
+  'en_US' => ['EnglishQuotes'],
   'en_GB' => ['EnglishQuotes'],
   'de_DE' => ['GermanQuotes'],
+  'de_AT' => ['GermanQuotes'],
+  'de_CH' => ['GermanQuotes'],
   'fr_FR' => ['FrenchQuotes', 'FrenchNoBreakSpace'],
   'fr_CA' => ['FrenchQuotes']
 ];
--- config/letterpress.php
+++ config/letterpress.php
@@ -1,12 +1,42 @@
 <?php
 return [
+  /**
+   * The locale used for all language dependent input fixes.
+   * Note that this can be overridden every single time
+   * simply with:
+   *
+   * ```php
+   *   $letterpress->press($input, ['letterpress.locale' => 'en_US'])
+   * ```
+   *
+   * Important: The provided locale must be an IETF language code.
+   **/
   'locale'   => 'en_GB',
 
-  'autoMarkdown' => true,
+  'markdown' => [
+    'enabled' => true,
 
-  // use "markdown extra" syntax instead of parsedown's defaults
-  // this only works if parsedown-extra was installed
-  'enableMarkdownExtra' => false,
+    // use "markdown extra" syntax instead of parsedown's defaults
+    // this only works if parsedown-extra was installed  
+    'useMarkdownExtra' => false
+  ],
 
-  'applyBlockquoteFix' => true,
+  'markup' => [
+    'enabeld' => true,
+
+    'blockQuoteFix' => true
+  ],
+
+  'media' => [
+    'enabled' => true
+  ],
+
+  'microtypography' => [
+    'enabled' => true,
+    'useDefaults' => true,
+
+    // you only need to add your additional fixers here, most of
+    // the stuff is handled by the defaults in config/jolitypo.php
+    'additionalFixers' => []
+  ]
 ];
