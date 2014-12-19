bb30b239f06bed79001dd068c9fc0f6cb7dcf5a9
Stefan Graupner <stefan.graupner@gmail.com>
2014-12-19 13:51:35 +0100

    facade for jolicode/jolitypo

--- src/integrations/Facade.php
+++ src/integrations/Facade.php
@@ -3,4 +3,7 @@
 interface Facade
 {
   public function __construct();
+  public function __get($property);
+  public function __set($property, $value);
+  public function __call($method, $args);
 }
--- src/integrations/TypoFixerFacade.php
+++ src/integrations/TypoFixerFacade.php
@@ -1,17 +1,75 @@
 <?php namespace EFrane\Letterpress\Facades;
 
 use EFrane\Letterpress\Config;
+use EFrane\Letterpress\LetterpressException;
+
 use JoliTypo\Fixer;
 
 class TypoFixerFacade implements Facade
 {
   protected $fixer = null;
-  protected $fixers = null;
+
+  protected $locale = '';
+  protected $fixers = [];
 
   public function __construct()
   {
-    if (!Config::get('letterpress.fixers'))
+    // get locale
+    if (($locale = Config::get('letterpress.locale')))
     {
+      $this->locale = $locale;
+    } else
+    {
+      throw new LetterpressException("Typography fixing requires a locale.");
     }
+
+    // defaults
+    if (Config::get('letterpress.microtypography.useDefaults'))
+    {
+      $this->fixers = Config::get('jolitypo.defaults');
+
+      // locale additions
+      $localeKey = sprintf('jolitypo.%s', $ths->locale);
+      if (Config::has($localeKey))
+      {
+        $this->fixers = array_merge($this->fixers, Config::get($localeKey));
+      }
+    }    
+
+    // user additions
+    $this->fixers = array_merge($this->fixers, Config::get('letterpress.fixers'));
+
+    if (count($this->fixers) === 0)
+      throw new LetterpressException("Typography fixing requires setting up fixers.");
+
+    $this->fixer = new Fixer($this->fixers);
+    $this->fixer->setLocale($this->locale);
+  }
+
+  public function __get($property)
+  {
+    if (strrpos($property, 'facade_') === 0)
+    {
+      return $this->{$property};
+    } else
+    {
+      return $this->fixer->{$property};
+    }
+  }
+
+  public function __set($property, $value)
+  {
+    if (strrpos($property, 'facade_') === 0)
+    {
+      $this->{$property} = $value;
+    } else
+    {
+      $this->fixer->{$property} = $value;
+    }
+  }
+
+  public function __call($method, $args)
+  {
+    return call_user_func_array([&$this->fixer, $method], $args);
   }
 }
