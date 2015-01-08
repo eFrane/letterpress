2dfc7b51c2aef33b8e86770cdae09a29433cbff3
Stefan Graupner <stefan.graupner@gmail.com>
2015-01-08 16:27:10 +0100

    More tests.

---
+++ tests/unit/BlockquoteModifierTest.php
@@ -0,0 +1,49 @@
+<?php
+
+use EFrane\Letterpress\Config;
+use EFrane\Letterpress\Letterpress;
+
+class BlockquoteModifierTest extends \Codeception\TestCase\Test
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    protected $lp;
+
+    protected function _before()
+    {
+        Config::init('config');
+        $this->lp = new Letterpress;
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testChangeMatchedBlockQuote()
+    {
+        $input = "> Feelings! I have no time for them, no chance of them.\n"
+               . "> I pass my whole life, miss, in turning an immense pecuniary Mangle.\n"
+               . "- Charles Dickens \"A Tale of Two Cities\"";
+
+        $expected = "<figure><p>Feel&shy;ings! I have no time for them, no chance of them.<br>\n"
+                  . "I pass my whole life, miss, in turn&shy;ing an immense pecu&shy;ni&shy;ary Mangle.</p>\n"
+                  . "\n"
+                  . "<figcaption>Charles Dick&shy;ens &ldquo;A Tale of Two Cities&rdquo;</figcaption></figure>";
+
+        $output = $this->lp->press($input);
+        $this->assertEquals($output, $expected);
+    }
+
+    public function testUnchangedNonMatchedBlockquote()
+    {
+        $input = "> This is just a normal blockquote.";
+
+        $expected = "<blockquote>\n<p>This is just a normal block&shy;quote.</p>\n</blockquote>";
+
+        $output = $this->lp->press($input);
+        $this->assertEquals($output, $expected);
+    }
+}
---
+++ tests/unit/HeadlineLevelModifierTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use EFrane\Letterpress\Config;
+use EFrane\Letterpress\Letterpress;
+
+class HeadlineLevelModifierTest extends \Codeception\TestCase\Test
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    protected $lp;
+
+    protected function _before()
+    {
+        Config::init('config');
+        $this->lp = new Letterpress;
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testHeadlineLevelChange()
+    {
+        $input    = "# This will remain a level 1 headline";
+        $expected = "<h1>This will remain a level 1 head&shy;line</h1>";
+
+        $output = $this->lp->press($input);
+        $this->assertEquals($expected, $output);
+
+        $input    = "# This will become a level 3 headline";
+        $expected = "<h3>This will become a level 3 head&shy;line</h3>";
+
+        $output = $this->lp->press($input, ['letterpress.markup.maxHeadlineLevel' => 3]);
+        $this->assertEquals($expected, $output);
+    }
+
+}
