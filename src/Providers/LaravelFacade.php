317b7bc58a531ae77de6857cb67a564f7c5ec2e7
Stefan Graupner <stefan.graupner@gmail.com>
2015-01-08 19:08:47 +0100

    Working on Laravel Integration.

---
+++ src/Providers/LaravelFacade.php
@@ -0,0 +1,8 @@
+<?php namespace EFrane\Letterpress\Providers;
+
+use Illuminate\Support\Facades\Facade;
+
+class LaravelFacade extends Facade
+{
+  protected static function getFacadeAccessor() { return 'letterpress'; }
+}
--- src/Providers/LaravelServiceProvider.php
+++ src/Providers/LaravelServiceProvider.php
@@ -3,19 +3,38 @@
 use EFrane\Letterpress\Letterpress;
 use EFrane\Letterpress\Config as LetterpressConfig;
 
-use Illuminate\Support\ServiceProvider
+use Illuminate\Support\ServiceProvider;
+use \Illuminate\Foundation\AliasLoader;
 
 class LaravelServiceProvider extends ServiceProvider
 {
   protected $defer = true;
 
+  public function boot()
+  {
+    $this->package('efrane/letterpress');
+  }
+
   public function register()
   {
+    LetterpressConfig::init(app_path().'/config/packages/efrane/letterpress');
+
     $this->app['letterpress'] = $this->app->share(function ($app) {
-      $config = LetterpressConfig::init();
-      $letterpress = new Letterpress($config);
-      
+      $letterpress = new Letterpress;
       return $letterpress;
     });
+
+    $this->app->bind('LetterpressConfig', 'EFrane\\Letterpress\\Config');
+
+    $this->app->booting(function()
+    {
+      $loader = AliasLoader::getInstance();
+      $loader->alias('Letterpress', 'EFrane\\Letterpress\\Providers\\LaravelFacade');
+    });
+  }
+
+  public function provides()
+  {
+    return ['letterpress'];
   }
 }
