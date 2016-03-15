2072dddf69dd087b1e30ceb410267cebef79b9f0
Stefan Graupner <stefan.graupner@gmail.com>
2016-03-16 00:30:11 +0100

    Added some duration related methods to lookups.

--- src/Markup/RichMedia/Lookup.php
+++ src/Markup/RichMedia/Lookup.php
@@ -38,4 +38,25 @@ class Lookup implements LookupInterface
     {
         return $this->adapter->getCode();
     }
+
+    public function getType()
+    {
+        return $this->adapter->getType();
+    }
+
+    public function hasDuration()
+    {
+        return in_array($this->getType(), ['video']);
+    }
+
+    public function getDuration()
+    {
+        if ($this->hasDuration()) {
+            // TODO: somehow fetch item duration
+            // NOTE: There might not be a generic way for this which would be a problem
+            return -1;
+        } else {
+            return 0;
+        }
+    }
 }
